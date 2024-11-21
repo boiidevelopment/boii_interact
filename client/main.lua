8d750776da6791bc01a9b65f15e47dcae294cacd
Case <90377400+CaseIRL@users.noreply.github.com>
2024-11-21 04:56:43 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -6,84 +6,8 @@ utils = exports.boii_utils:get_utils()
 --- @section Constants
 
 local RESOURCE_NAME <const> = GetCurrentResourceName()
-local DUI_RANGE <const> = 1.2
+local DUI_RANGE <const> = 1.5
 local PROXIMITY_CHECK_INTERVAL <const> = 100
-
-local KEYS <const> = {
-    ['enter'] = 191,
-    ['escape'] = 322,
-    ['backspace'] = 177,
-    ['tab'] = 37,
-    ['arrowleft'] = 174,
-    ['arrowright'] = 175,
-    ['arrowup'] = 172,
-    ['arrowdown'] = 173,
-    ['space'] = 22,
-    ['delete'] = 178,
-    ['insert'] = 121,
-    ['home'] = 213,
-    ['end'] = 214,
-    ['pageup'] = 10,
-    ['pagedown'] = 11,
-    ['leftcontrol'] = 36,
-    ['leftshift'] = 21,
-    ['leftalt'] = 19,
-    ['rightcontrol'] = 70,
-    ['rightshift'] = 70,
-    ['rightalt'] = 70,
-    ['numpad0'] = 108,
-    ['numpad1'] = 117,
-    ['numpad2'] = 118,
-    ['numpad3'] = 60,
-    ['numpad4'] = 107,
-    ['numpad5'] = 110,
-    ['numpad6'] = 109,
-    ['numpad7'] = 117,
-    ['numpad8'] = 111,
-    ['numpad9'] = 112,
-    ['numpad+'] = 96,
-    ['numpad-'] = 97,
-    ['numpadenter'] = 191,
-    ['numpad.'] = 108,
-    ['f1'] = 288,
-    ['f2'] = 289,
-    ['f3'] = 170,
-    ['f4'] = 168,
-    ['f5'] = 166,
-    ['f6'] = 167,
-    ['f7'] = 168,
-    ['f8'] = 169,
-    ['f9'] = 56,
-    ['f10'] = 57,
-    ['a'] = 34,
-    ['b'] = 29,
-    ['c'] = 26,
-    ['d'] = 30,
-    ['e'] = 46,
-    ['f'] = 49,
-    ['g'] = 47,
-    ['h'] = 74,
-    ['i'] = 27,
-    ['j'] = 36,
-    ['k'] = 311,
-    ['l'] = 182,
-    ['m'] = 244,
-    ['n'] = 249,
-    ['o'] = 39,
-    ['p'] = 199,
-    ['q'] = 44,
-    ['r'] = 45,
-    ['s'] = 33,
-    ['t'] = 245,
-    ['u'] = 303,
-    ['v'] = 0,
-    ['w'] = 32,
-    ['x'] = 73,
-    ['y'] = 246,
-    ['z'] = 20,
-    ['mouse1'] = 24,
-    ['mouse2'] = 25
-}
 
 --- @section Tables
 
@@ -135,8 +59,11 @@ end
 --- Adds a new zone to the DUI system
 --- @param options table: A table containing zone options
 local function add_zone(options)
-    if not options.id or (not options.coords and not options.entity) or not options.header or not options.keys then print('Error: Missing required fields for zone.')  return end
-    dui_locations[options.id] = {
+    local invoking_resource = GetInvokingResource()
+    if not invoking_resource then print('Error: Invoking resource could not be determined.') return end
+    if not options.id or (not options.coords and not options.entity) or not options.header or not options.keys then print('Error: Missing required fields for zone.') return end
+    dui_locations[invoking_resource] = dui_locations[invoking_resource] or {}
+    dui_locations[invoking_resource][options.id] = {
         id = options.id,
         coords = options.coords,
         entity = options.entity,
@@ -223,7 +150,7 @@ end
 local function handle_key_presses(location)
     for _, key_data in ipairs(location.keys) do
         local key_lower = string.lower(key_data.key)
-        local key_control = KEYS[key_lower]
+        local key_control = utils.keys.get_key(key_lower)
         if IsControlJustReleased(0, key_control) then
             perform_key_action(key_data, location)
         end
@@ -326,6 +253,25 @@ local function handle_dui_render(location)
     handle_key_presses(location)
 end
 
+--- Removes all zones for a specific resource
+--- @param resource_name string: The name of the resource
+local function clear_zones_for_resource(resource_name)
+    if dui_locations[resource_name] then
+        for zone_id, _ in pairs(dui_locations[resource_name]) do
+            manage_zone(zone_id, 'remove')
+        end
+        dui_locations[resource_name] = nil
+    end
+    if dui_objects[resource_name] then
+        for _, dui_data in pairs(dui_objects[resource_name]) do
+            DestroyDui(dui_data.dui_object)
+        end
+        dui_objects[resource_name] = nil
+    end
+end
+
+exports('clear_zones', clear_zones_for_resource)
+
 --- @section Threads
 
 --- Periodically check proximity to zones
@@ -333,9 +279,11 @@ CreateThread(function()
     while true do
         Wait(PROXIMITY_CHECK_INTERVAL)
         local player_coords = GetEntityCoords(PlayerPedId())
-        for _, location in pairs(dui_locations) do
-            local distance = #(player_coords - vector3(location.coords.x, location.coords.y, location.coords.z))
-            handle_proximity(location, distance)
+        for resource_name, locations in pairs(dui_locations) do
+            for _, location in pairs(locations) do
+                local distance = #(player_coords - vector3(location.coords.x, location.coords.y, location.coords.z))
+                handle_proximity(location, distance)
+            end
         end
     end
 end)
@@ -344,11 +292,14 @@ end)
 CreateThread(function()
     while true do
         Wait(0)
-        for _, location in pairs(dui_locations) do
-            if location.in_proximity then
-                handle_dui_render(location)
+        for resource_name, locations in pairs(dui_locations) do
+            for _, location in pairs(locations) do
+                if location.in_proximity then
+                    handle_dui_render(location)
+                end
             end
         end
+
         ClearDrawOrigin()
     end
 end)
